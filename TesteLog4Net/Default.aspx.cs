1f52d69bcdfed1c38bbd7b8220344b6b6ba404bd
Diogo-PC\Diogo <diogodrsa@gmail.com>
2017-12-30 14:36:46 -0300

    Primeiro Commit do projeto de Log4Net

---
+++ TesteLog4Net/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TesteLog4Net
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            XmlConfigurator.Configure();
+            ILog logger = log4net.LogManager.GetLogger(typeof(_Default));
+
+            logger.Debug("Debug message");
+            logger.Info("Info message");
+            logger.Warn("Warning message");
+            logger.Error("Error message");
+            logger.Fatal("Fatal message");
+        }
+    }
+}
